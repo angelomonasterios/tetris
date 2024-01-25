aa73bc9c80c8dc6f364ca966fad835fa19b1499c
angelomonasterios <angelomonasterios@gmail.com>
2024-01-25 10:54:10 -0300

    Refactor code for row processing and figure rotation
    The update greatly refactors the code for processing rows in 'removeLines.ts' and 'observeRows.ts' as well as for figure rotation in 'rotate/index.ts'. Functions have been broken down into smaller, reusable parts which improves code maintainability, readability, and efficiency.

--- src/test/removeLines/removeLines.ts
+++ src/test/removeLines/removeLines.ts
@@ -4,19 +4,21 @@ import {observeRows} from "../../observeRows";
 const removeLinesTest = () => {
     const processRow = (rowIndex: number, adjustment: number) => {
         const rowChildren = document.getElementsByClassName('row')[rowIndex].childNodes;
-        const midPoint = globalVars.sizeGameAreaX / 2;
+        const midPoint = Math.round(globalVars.sizeGameAreaX / 2);
+
         rowChildren.forEach((x: any, key: number) => {
-            if (key < (midPoint - adjustment) || (midPoint + adjustment) < key) {
+            if (key <= (midPoint - adjustment) || (midPoint + adjustment) <= key) {
                 x["classList"].add('boxFixed');
             }
         });
     }
 
+    processRow(14, 4);
+    processRow(15, 2);
+    processRow(16, 0);
     processRow(17, 2);
-    processRow(16, 3);
-    processRow(16, 4);
-    processRow(18, 0);
-    processRow(19, 0);
+    processRow(18, 1);
+    processRow(19, 1);
 
     setTimeout(observeRows, 2000 )
 
